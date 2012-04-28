5ff1c95669d08a83f5a928279fa9ec6624342966
James Robinson <admin@robinjam.net>
2012-04-28 23:48:39 +0100

    Improve Scene API

--- src/main/java/alsoknownasthemanatees/cosmoskerfuffle/scene/Scene.java
+++ src/main/java/alsoknownasthemanatees/cosmoskerfuffle/scene/Scene.java
@@ -1,13 +1,18 @@
 package alsoknownasthemanatees.cosmoskerfuffle.scene;
 
 import java.awt.Graphics2D;
+import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.Stack;
 
-public interface Scene extends KeyListener {
+public class Scene implements KeyListener {
 	
 	public static final Stack<Scene> stack = new Stack<Scene>();
 	
-	public abstract void paint(Graphics2D g);
+	public void paint(Graphics2D g) {}
+	
+	public void keyTyped(KeyEvent ke) {}
+	public void keyPressed(KeyEvent ke) {}
+	public void keyReleased(KeyEvent ke) {}
 	
 }
--- src/main/java/alsoknownasthemanatees/cosmoskerfuffle/scene/SplashScreen.java
+++ src/main/java/alsoknownasthemanatees/cosmoskerfuffle/scene/SplashScreen.java
@@ -5,22 +5,14 @@ import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.event.KeyEvent;
 
-public class SplashScreen implements Scene {
+public class SplashScreen extends Scene {
 
+	@Override
 	public void paint(Graphics2D g) {
 		g.setBackground(Color.BLACK);
 		g.clearRect(0, 0, GamePanel.instance.getWidth(), GamePanel.instance.getHeight());
 		g.setColor(Color.WHITE);
 		g.drawString("Press any key", 0, 10);
-	}
-
-	public void keyTyped(KeyEvent ke) {
-	}
-
-	public void keyPressed(KeyEvent ke) {
-	}
-
-	public void keyReleased(KeyEvent ke) {
 	}
 	
 }
