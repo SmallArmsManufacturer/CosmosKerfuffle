dc02aba8b78517a015b271d217f3bda6cc3527ed
James Robinson <admin@robinjam.net>
2012-04-29 02:40:54 +0100

    Rotateamajig

--- src/main/java/alsoknownasthemanatees/cosmoskerfuffle/graphics/Sprite.java
+++ src/main/java/alsoknownasthemanatees/cosmoskerfuffle/graphics/Sprite.java
@@ -1,6 +1,7 @@
 package alsoknownasthemanatees.cosmoskerfuffle.graphics;
 
 import java.awt.Graphics2D;
+import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import javax.imageio.ImageIO;
@@ -9,7 +10,7 @@ public class Sprite {
 	
 	public static final int SIZE = 16;
 	private static BufferedImage spriteSheet;
-	
+	double total = 0;
 	private int x, y;
 	
 	static {
@@ -30,10 +31,16 @@ public class Sprite {
 	}
 	
 	public void paint(Graphics2D g, int x, int y, int size) {
+		AffineTransform t = g.getTransform();
+		g.translate(x + SIZE / 2, y + SIZE / 2);
+		total += 0.01;
+		g.rotate(total);
+		g.translate(-x - SIZE / 2,-y - SIZE / 2);
 		g.drawImage(spriteSheet,
 					x,      y,      x + size,      y + size,      // Destination
 					this.x, this.y, this.x + SIZE, this.y + SIZE, // Source
 					null);
+		g.setTransform(t);
 	}
 	
 	public static enum Type { PARROT, MANTA_RAY, TURTLE }
