8036517bb92b6d0bfdcb9af332741b7ed2c9e3b1
James Robinson <admin@robinjam.net>
2012-04-29 03:42:39 +0100

    Add box and stuff

--- src/main/java/alsoknownasthemanatees/cosmoskerfuffle/entity/Entity.java
+++ src/main/java/alsoknownasthemanatees/cosmoskerfuffle/entity/Entity.java
@@ -1,7 +1,6 @@
 package alsoknownasthemanatees.cosmoskerfuffle.entity;
 
 import alsoknownasthemanatees.cosmoskerfuffle.graphics.Sprite;
-import java.awt.Graphics2D;
 import java.util.Random;
 
 public class Entity {
@@ -11,12 +10,18 @@ public class Entity {
 	
 	public Entity(Sprite sprite) {
 		this.sprite = sprite;
-		Random r = new Random();
+		x = 500;
+		y = 500;
 	}
 	
 	public void thrust (double dt) {
 		vx += Math.sin(rotation) * dt * 100;
 		vy -= Math.cos(rotation) * dt * 100;
+		
+		if (vx * vx + vy * vy > 10000) {
+			vx *= 0.8;
+			vy *= 0.8;
+		}
 	}
 	
 }
--- src/main/java/alsoknownasthemanatees/cosmoskerfuffle/scene/MainScene.java
+++ src/main/java/alsoknownasthemanatees/cosmoskerfuffle/scene/MainScene.java
@@ -9,33 +9,40 @@ import java.awt.event.KeyEvent;
 import java.awt.geom.AffineTransform;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class MainScene extends Scene {
 	
+	public static int NUM_STARS = 1000;
+	
 	private List<Entity> entities = new ArrayList<Entity>();
+	private List<Star> stars = new ArrayList<Star>();
 	private Entity player1, player2;
 	
 	public MainScene() {
 		player1 = new Entity(new Sprite(Sprite.Type.PARROT));
 		player2 = new Entity(new Sprite(Sprite.Type.TURTLE));
+		player2.x += 100;
 		entities.add(player1);
 		entities.add(player2);
+		for (int i = 0; i < NUM_STARS; i++)
+			stars.add(new Star());
 	}
 	
 	@Override
 	public void update(double dt) {
-			if (keys.contains(KeyEvent.VK_UP))
+			if (keys.contains(KeyEvent.VK_W))
 				player1.thrust(dt);
-			if (keys.contains(KeyEvent.VK_LEFT))
+			if (keys.contains(KeyEvent.VK_A))
 				player1.rotation -= Math.PI * dt;
-			if (keys.contains(KeyEvent.VK_RIGHT))
+			if (keys.contains(KeyEvent.VK_D))
 				player1.rotation += Math.PI * dt;
 			
-			if (keys.contains(KeyEvent.VK_W))
+			if (keys.contains(KeyEvent.VK_UP))
 				player2.thrust(dt);
-			if (keys.contains(KeyEvent.VK_A))
+			if (keys.contains(KeyEvent.VK_LEFT))
 				player2.rotation -= Math.PI * dt;
-			if (keys.contains(KeyEvent.VK_D))
+			if (keys.contains(KeyEvent.VK_RIGHT))
 				player2.rotation += Math.PI * dt;
 
 		for (Entity e : entities) {
@@ -43,6 +50,8 @@ public class MainScene extends Scene {
 			e.y += e.vy * dt;
 			if (e.x < 0) e.vx = Math.abs(e.vx);
 			if (e.y < 0) e.vy = Math.abs(e.vy);
+			if (e.x > 1000 - Sprite.SIZE) e.vx = -Math.abs(e.vx);
+			if (e.y > 1000 - Sprite.SIZE) e.vy = -Math.abs(e.vy);
 		}
 	}
 
@@ -70,9 +79,27 @@ public class MainScene extends Scene {
 		t.scale(zoom, zoom);
 		g.setTransform(t);
 		
+		g.setColor(Color.WHITE);
+		g.drawRect(- (int) center_x, - (int) center_y, 1000, 1000);
+		for (Star star : stars) {
+			g.fillRect((int) (star.x - center_x), (int) (star.y - center_y), 1, 1);
+		}
+		
 		for (Entity e : entities) {
 			e.sprite.paint(g, e.rotation, (int) (e.x - center_x), (int) (e.y - center_y), Sprite.SIZE);
 		}
+	}
+	
+	public static class Star {
+		
+		public double x, y;
+		
+		public Star() {
+			Random r = new Random();
+			this.x = r.nextDouble() * 1000;
+			this.y = r.nextDouble() * 1000;
+		}
+		
 	}
 	
 }
