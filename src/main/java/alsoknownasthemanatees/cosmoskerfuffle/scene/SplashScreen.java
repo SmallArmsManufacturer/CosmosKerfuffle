dda176e8f415d9e308be7f766544036cf26df825
James Robinson <admin@robinjam.net>
2012-04-29 06:40:03 +0100

    Add "press any key" message to splash screen

--- src/main/java/alsoknownasthemanatees/cosmoskerfuffle/scene/SplashScreen.java
+++ src/main/java/alsoknownasthemanatees/cosmoskerfuffle/scene/SplashScreen.java
@@ -4,15 +4,35 @@ import alsoknownasthemanatees.cosmoskerfuffle.GamePanel;
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.event.KeyEvent;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 
 public class SplashScreen extends Scene {
+	
+	private BufferedImage pressAnyKey;
+	private double elapsedTime;
+	
+	public SplashScreen() {
+		try {
+			pressAnyKey = ImageIO.read(SplashScreen.class.getResource("/pressAnyKey.png"));
+		} catch (IOException ex) {
+			ex.printStackTrace(System.err);
+			System.exit(1);
+		}
+	}
+	
+	@Override
+	public void update(double dt) {
+		elapsedTime += dt;
+	}
 
 	@Override
 	public void paint(Graphics2D g) {
-		g.setBackground(Color.BLACK);
+		g.setBackground(Color.WHITE);
 		g.clearRect(0, 0, GamePanel.instance.getWidth(), GamePanel.instance.getHeight());
-		g.setColor(Color.WHITE);
-		g.drawString("Press any key", 0, 10);
+		if ((int) elapsedTime % 2 == 0)
+			g.drawImage(pressAnyKey, GamePanel.instance.getWidth() / 2 - pressAnyKey.getWidth() / 2, GamePanel.instance.getHeight() / 2 - pressAnyKey.getHeight() / 2, null);
 	}
 	
 	@Override
